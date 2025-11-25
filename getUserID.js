91122527987e5a72d4a80a557d6e85d08879a66b
SLWadmin <shorterlakewoodshoa@yahoo.com>
2025-11-25 18:30:32 -0500

    Create getUserID.js initial file

---
+++ getUserID.js
@@ -0,0 +1,23 @@
+function getUserID(uname,pword,uAccounts) {
+	   
+//	 <script src = "classes.js">             </script>//
+	   
+	 console.log(" validate user");
+     console.log(" number of user is " + uAccounts.length);	
+     for(let i = 0; i< uAccounts.length; i++) {
+          console.log(i + " uname is " + uAccounts[i].username + "   " + uAccounts[i].password);
+	 }		  
+     let userId = -1;
+
+	   
+	 for(let i = 0; i < uAccounts.length; i++){
+	    if((uname == uAccounts[i].username) && (pword == uAccounts[i].password)) {
+            userId = i;
+			return userId;
+		   } 
+	 }
+	//  failed login  //
+	
+	 
+    return userId;
+	 }
