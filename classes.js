610f89e7e220bc336e44f13002c1d1e4c014c62d
SLWadmin <shorterlakewoodshoa@yahoo.com>
2026-07-15 15:43:42 -0400

    Modify account constructor to include linkaddr
    Added linkaddr parameter to account constructor.

--- classes.js
+++ classes.js
@@ -1,7 +1,8 @@
 class account{
-		constructor(username,password,role) {
+		constructor(username,password,role,linkaddr) {
 			this.username = username;
 			this.password = password;
 			this.role     = role;
+			this.linkaddr = linksddr;
 		}
 	}
