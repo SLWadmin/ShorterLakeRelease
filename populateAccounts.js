1c6544c2f4e57d38884ffaf2368051f7a9e57034
SLWadmin <shorterlakewoodshoa@yahoo.com>
2026-07-16 21:12:04 -0400

    Add document links to user accounts
    Updated user accounts to include document links.

--- populateAccounts.js
+++ populateAccounts.js
@@ -21,24 +21,24 @@ function populateAccounts() {
     accounts[15] = new account("mommajane88@gmail.com"      , "9097","user",16);
     accounts[16] = new account("mertzjef@gmail.com"         , "9097","user",17);
     accounts[17] = new account("sandraraye68@gmail.com"     , "9100","user",18);
-    accounts[18] = new account("raineycar87@yahoo.com"      , "9121","user",19);
+    accounts[18] = new account("raineycar87@yahoo.com"      , "9121","user","https://docs.google.com/document/d/17eR4Rq2mxbXXWGOtTp4P8HDHriILRIu-6V-1sxPgGaU/edit?usp=sharing");
     accounts[19] = new account("pepejoanna@gmail.com"       , "9132","user",20);
     accounts[20] = new account("dveith@outlook.com"         , "9133","user",21);
     accounts[21] = new account("shaunna.schut@gmail.com"    , "9136","user",22);
-    accounts[22] = new account("overholserr@msn.com"        , "9147","user",23);
+    accounts[22] = new account("overholserr@msn.com"        , "9147","user","https://docs.google.com/document/d/1oHJ5gghLRgHeuwI_EHIt89PXwW6s6vyEIwAyi4rYJdk/edit?usp=sharing");
 	
     accounts[23] = new account("barleys@centurylink.net"    , "9154","user",24);
     accounts[24] = new account("bonniebarley@gmail.com"     , "9154","user",25);
     accounts[25] = new account("jandkpatten@aol.com"        , "9159","user",26);
     accounts[26] = new account("ctorreyamazed@gmail.com"    , "9160","user",27);
     accounts[27] = new account("petetorrey52@gmail.com"     , "9160","user",28);
-    accounts[28] = new account("ajmaniaci77@gmail.com"      , "9177","user",29);
-    accounts[29] = new account("stevebalogh@sbcglobal.net"  , "9197","user",30);
+    accounts[28] = new account("ajmaniaci77@gmail.com"      , "9177","user","https://docs.google.com/document/d/16fCQjXwypSnjulhd2bmeT025ynCHYXAp7u9r8nk4F6o/edit?usp=sharing");
+    accounts[29] = new account("stevebalogh@sbcglobal.net"  , "9197","user","https://docs.google.com/document/d/1BpaK5J_P8V3s2-ojrlnUmuy4JHQlbg4-BG8EtPEuBjo/edit?usp=sharing");
     accounts[30] = new account("subs@prodigy.net"           , "9201","user",31);
     accounts[31] = new account("suziowen60@gmail.com"       , "9210","user",32);
     accounts[32] = new account("brodydog11@yahoo.com"       , "9216","user",33);
 	
-    accounts[33] = new account("looker_lisa@yahoo.com"      , "9237","user",34);
+    accounts[33] = new account("looker_lisa@yahoo.com"      , "9237","user","https://docs.google.com/document/d/1Xd8Cr_annXiXGMZZPIvDbSpnzqPnX85WLJmy1VomBac/edit?usp=sharing");
     accounts[34] = new account("tylertfenton@gmail.com"     , "9239","user",35);
     accounts[35] = new account("vanhoutenadam@gmail.com"    , "9242","user",36);
     accounts[36] = new account("cathy.shoemaker@rehmann.com", "9263","user",37);
