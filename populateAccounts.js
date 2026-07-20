275ccae77799fb770d3a5372d20aafce46e10af6
SLWadmin <shorterlakewoodshoa@yahoo.com>
2026-07-20 12:31:50 -0400

    Replace numeric values with document links in accounts
    Updated account entries to replace numeric values with corresponding document links.

--- populateAccounts.js
+++ populateAccounts.js
@@ -5,18 +5,18 @@ function populateAccounts() {
 
     accounts[0]  = new account("sonnyferrari@yahoo.com"     , "9179","officer","https://docs.google.com/document/d/1kK75YD7tzQyilxlhh9XIDg8wptUzW7hPwZt8k-fNZAU/edit?usp=sharing");
     accounts[1]  = new account("sadavey2@yahoo.com"         , "9176","officer","https://docs.google.com/document/d/1zv20Dblv-Rs2IQGYl9CmCslmNaGBKiH9EaADK1f4jBY/edit?usp=sharing");
-    accounts[2]  = new account("dseward77@yahoo.com"        , "9041","officer",2);
+    accounts[2]  = new account("dseward77@yahoo.com"        , "9041","officer","https://docs.google.com/document/d/1zv20Dblv-Rs2IQGYl9CmCslmNaGBKiH9EaADK1f4jBY/edit?usp=sharing");
     accounts[3]  = new account("chrissheffer@chartermi.net" , "9190","officer",18);
     accounts[4]  = new account("sreed1@mhc.net"             , "9015","user","https://docs.google.com/document/d/1mFszTUQ3URwljWpiG9klL2rybxc3hVpa0NsFqDg5D1Y/edit?usp=sharing");
-    accounts[5]  = new account("shc57@hotmail.com"          , "9018","user",49);
+    accounts[5]  = new account("shc57@hotmail.com"          , "9018","user","https://docs.google.com/document/d/1RgwEsfxJoFpg71NvYAvm6r5WNJUFb2Hcdg9g5HRLUJ8/edit?usp=sharing");
     accounts[6]  = new account("jasonlpierce21@gmail.com"   , "9021","user",0);
     accounts[7]  = new account("sholzworth@gmail.com"       , "9039","user",26);
-    accounts[8]  = new account("suzannekunze@outlook.com"   , "9042","user",48);
+    accounts[8]  = new account("suzannekunze@outlook.com"   , "9042","user","https://docs.google.com/document/d/1jhVP5Uadh8Dy7VCmz6hRaTA6GuexiBbbbV15WP8avkY/edit?usp=sharing");
     accounts[9]  = new account("mccram@gmail.com"           , "9061","user","https://docs.google.com/document/d/1ngWEqXtXyMr1-1DHR3zVsr-3Hx2G1LEVqsfk108FnqU/edit?usp=sharing");
     accounts[10] = new account("94moose@gmail.com"          , "9061","user","https://docs.google.com/document/d/1ngWEqXtXyMr1-1DHR3zVsr-3Hx2G1LEVqsfk108FnqU/edit?usp=sharing");
     accounts[11] = new account("leslie9068@hotmail.com"     , "9068","user",22);
-    accounts[12] = new account("jruggles@ymail.com"         , "9070","user",47);
-    accounts[13] = new account("rgruggles@ymail.com"        , "9070","user",47);
+    accounts[12] = new account("jruggles@ymail.com"         , "9070","user","https://docs.google.com/document/d/1tyfovqVzUn-M70sUQf-flEslmHfImRJDmvoKBrA3rI4/edit?usp=sharing");
+    accounts[13] = new account("rgruggles@ymail.com"        , "9070","user","https://docs.google.com/document/d/1tyfovqVzUn-M70sUQf-flEslmHfImRJDmvoKBrA3rI4/edit?usp=sharing");
     accounts[14] = new account("pikunas@gmail.com"          , "9075","user",27);
     accounts[15] = new account("mommajane88@gmail.com"      , "9097","user","https://docs.google.com/document/d/1uWubPUNxYcwyfkzz33ueaGkA17uPDIQngle8BdMwmHw/edit?usp=sharing");
     accounts[16] = new account("mertzjef@gmail.com"         , "9097","user","https://docs.google.com/document/d/1uWubPUNxYcwyfkzz33ueaGkA17uPDIQngle8BdMwmHw/edit?usp=sharing");
@@ -24,36 +24,41 @@ function populateAccounts() {
     accounts[18] = new account("raineycar87@yahoo.com"      , "9121","user","https://docs.google.com/document/d/17eR4Rq2mxbXXWGOtTp4P8HDHriILRIu-6V-1sxPgGaU/edit?usp=sharing");
     accounts[19] = new account("pepejoanna@gmail.com"       , "9132","user",20);
     accounts[20] = new account("dveith@outlook.com"         , "9133","user",29);
-    accounts[21] = new account("shaunna.schut@gmail.com"    , "9136","user",45);
+    accounts[21] = new account("shaunna.schut@gmail.com"    , "9136","user","https://docs.google.com/document/d/1KslFRBp2nbY6GzOBpLT0u7JJuSGBU068zrq4tGMQgLs/edit?usp=sharing");
     accounts[22] = new account("overholserr@msn.com"        , "9147","user","https://docs.google.com/document/d/1oHJ5gghLRgHeuwI_EHIt89PXwW6s6vyEIwAyi4rYJdk/edit?usp=sharing");
 	
-    accounts[23] = new account("barleys@centurylink.net"    , "9154","user",44);
-    accounts[24] = new account("bonniebarley@gmail.com"     , "9154","user",44);
-    accounts[25] = new account("jandkpatten@aol.com"        , "9159","user",30);
+    accounts[23] = new account("barleys@centurylink.net"    , "9154","user","https://docs.google.com/document/d/14bmXaT0nALo7tjB3tfVCUndbB4WW1QHMhN3DXUS7qco/edit?usp=sharing");
+    accounts[24] = new account("bonniebarley@gmail.com"     , "9154","user","https://docs.google.com/document/d/14bmXaT0nALo7tjB3tfVCUndbB4WW1QHMhN3DXUS7qco/edit?usp=sharing");
+    accounts[25] = new account("jandkpatten@aol.com"        , "9159","user","https://docs.google.com/document/d/1bXaYEfszokb9HwnMOl4fx4DrAa_4w8FjexkF9Xsc9YM/edit?usp=sharing");
     accounts[26] = new account("ctorreyamazed@gmail.com"    , "9160","user",19);
     accounts[27] = new account("petetorrey52@gmail.com"     , "9160","user",19);
     accounts[28] = new account("ajmaniaci77@gmail.com"      , "9177","user","https://docs.google.com/document/d/16fCQjXwypSnjulhd2bmeT025ynCHYXAp7u9r8nk4F6o/edit?usp=sharing");
     accounts[29] = new account("stevebalogh@sbcglobal.net"  , "9197","user","https://docs.google.com/document/d/1BpaK5J_P8V3s2-ojrlnUmuy4JHQlbg4-BG8EtPEuBjo/edit?usp=sharing");
-    accounts[30] = new account("subs@prodigy.net"           , "9201","user",32);
-    accounts[31] = new account("suziowen60@gmail.com"       , "9210","user",42);
+    accounts[30] = new account("subs@prodigy.net"           , "9201","user","https://docs.google.com/document/d/1uSs4ptSL70RJ_RPU-0TTbbbYQGONYTb95jm_N6ujagY/edit?usp=sharing");
+    accounts[31] = new account("suziowen60@gmail.com"       , "9210","user","https://docs.google.com/document/d/12-RpHEJ1V9MD0Fk1v8nIu5Jc907QmGnQ_F3c8RP8_XM/edit?usp=sharing");
     accounts[32] = new account("brodydog11@yahoo.com"       , "9216","user",17);
 	
     accounts[33] = new account("looker_lisa@yahoo.com"      , "9237","user","https://docs.google.com/document/d/1Xd8Cr_annXiXGMZZPIvDbSpnzqPnX85WLJmy1VomBac/edit?usp=sharing");
     accounts[34] = new account("tylertfenton@gmail.com"     , "9239","user",0);
     accounts[35] = new account("vanhoutenadam@gmail.com"    , "9242","user",16);
     accounts[36] = new account("cathy.shoemaker@rehmann.com", "9263","user","https://docs.google.com/document/d/1tn1kPU9iIpJ-dMkf_DTrMedAQV-hoeMGEt1G4_aaq3w/edit?usp=sharing");
-    accounts[37] = new account("tinamw03@yahoo.com"         , "9270","user",40);
-    accounts[38] = new account("myerske9@gmail.com"         , "9271","user",34);
-    accounts[39] = new account("bgsmiles@charter.net"       , "9290","user",39);
+    accounts[37] = new account("tinamw03@yahoo.com"         , "9270","user","https://docs.google.com/document/d/1lpbRQZQn2D7zocq2UHIGYSIAg0Iz-ipDFCXJk615Pmw/edit?usp=sharing");
+    accounts[38] = new account("myerske9@gmail.com"         , "9271","user","https://docs.google.com/document/d/1Fw1YrQo-3-REb1VLPNoXmFeMCU0oA0kC7fpQTXecrxU/edit?usp=sharing");
+    accounts[39] = new account("bgsmiles@charter.net"       , "9290","user","https://docs.google.com/document/d/1g_6zetRkoNIjXmF-anuVx-4gobFOS14Qy2GsYz4YqZs/edit?usp=sharing");
     accounts[40] = new account("laurendraplin@yahoo.com"    , "9303","user",12);
-    accounts[41] = new account("jasman25@hotmail.com"       , "9307","user",35);
-    accounts[42] = new account("melpomuse@hotmail.com"      , "9310","user",38);
-    accounts[43] = new account("htbig@yahoo.com"            , "9334","user",37);
+    accounts[41] = new account("jasman25@hotmail.com"       , "9307","user","https://docs.google.com/document/d/1SdJn1hHQN3gWLBPjStE0liWFI3Kcyw2wFcwD6vm36tw/edit?usp=sharing");
+    accounts[42] = new account("melpomuse@hotmail.com"      , "9310","user","https://docs.google.com/document/d/1XsGg9H0wnQYk8OnMLu1nHjje3wd5nln4lAqJEuKbCDM/edit?usp=sharing");
+    accounts[43] = new account("htbig@yahoo.com"            , "9334","user","https://docs.google.com/document/d/1-P0O4gdZ0oijVoHRfnifd4zF5em9eaVBfGykAa5h0EY/edit?usp=sharing");
     accounts[44] = new account("klhocking@gmail.com"        , "9340","user","https://docs.google.com/document/d/1YNFiW0bFouPDcCM2geqOo2wW_pTlkekxFNkku0tWut0/edit?usp=sharing");
-    accounts[45] = new account("bennsvette@hotmail.com"     , "9360","user",36);
+    accounts[45] = new account("bennsvette@hotmail.com"     , "9360","user","https://docs.google.com/document/d/1WcmDmsQjY0Tgh9NV4N1zXDM1CPqkM0LvADmc_QUYG5M/edit?usp=sharing");
     accounts[46] = new account("angmillard@hotmail.com"     , "9360","user",0);
-    accounts[47] = new account("idoctros@msn.com"           , "9437","user",48);
+    accounts[47] = new account("idoctros@msn.com"           , "9437","user","https://docs.google.com/document/d/1jhVP5Uadh8Dy7VCmz6hRaTA6GuexiBbbbV15WP8avkY/edit?usp=sharing");
     accounts[48] = new account("jshopshear@lamar.com"       , "9440","user","https://docs.google.com/document/d/1xVRk93HgTF8mNkhgzXqpkBJ9dQwZqib86PVNI3-O2Bo/edit?usp=sharing");
+	accounts[49] = new account("co-owner32_1"               , "9201","user","https://docs.google.com/document/d/1uSs4ptSL70RJ_RPU-0TTbbbYQGONYTb95jm_N6ujagY/edit?usp=sharing");
+	accounts[50] = new account("co-owner32_2"               , "9201","user","https://docs.google.com/document/d/1uSs4ptSL70RJ_RPU-0TTbbbYQGONYTb95jm_N6ujagY/edit?usp=sharing");
+	accounts[51] = new account("co-owner30_1"               , "9159","user","https://docs.google.com/document/d/1bXaYEfszokb9HwnMOl4fx4DrAa_4w8FjexkF9Xsc9YM/edit?usp=sharing");
+	accounts[52] = new account("co-owner30_2"               , "9159","user","https://docs.google.com/document/d/1bXaYEfszokb9HwnMOl4fx4DrAa_4w8FjexkF9Xsc9YM/edit?usp=sharing");
+	
 	
 //	alert(" just read up accounts - lenght = " + accounts.length);   //
 	return accounts;
